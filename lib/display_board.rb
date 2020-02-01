cc3e736f5513c6d7065aa417339829a0a1e19b34
Gracie McGuire <gracie.mcguire@flatironschool.com>
2020-02-01 18:48:49 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,11 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 
-turn_1 = []
+board = [" "," "," "," "," "," "," "," "," "]
 
 
 def display_board(board)
-  puts ' #{the_board[0]} | #{} | #{} '
+  puts ' #{board[0]} | #{board[1]} | #{board[2]} '
   puts '-----------'
   puts ' #{} | #{} | #{} '
   puts '-----------'
